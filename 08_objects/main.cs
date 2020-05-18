566c5bb0b96f4a6fa5bfa028ed565b504d20fdb2
Nikos Mouzakitis <nikos@virtualopensystems.com>
2020-05-18 03:01:45 +0200

    08,09
    Signed-off-by: Nikos Mouzakitis <nikos@virtualopensystems.com>

---
+++ 08_objects/main.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Hello
+{
+	static public void Main()
+	{
+		// objects are the parents of all other types in the .NET Frameworok.
+		object a, b;
+		a = 5;
+		b = "ep";
+	
+		Console.WriteLine("Start");
+		Console.WriteLine("value of a: "+a);
+		Console.WriteLine("value of b: "+b);
+
+	}	
+}
---
+++ 09_try_catch_exception/main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO; //included for StreamReader.
+
+public class HelloExceptions
+{
+	static public void Main()
+	{
+		Console.WriteLine("Start");
+		string fileName = "wrong.txt"; //if file doesn't exist runtime error.
+		ReadFile(fileName);
+	}	
+
+	static void ReadFile(string fileName)
+	{
+		try {
+			TextReader reader = new StreamReader(fileName);
+			string line = reader.ReadLine();
+			Console.WriteLine(line);
+			reader.Close();
+		} catch(FileNotFoundException fnfe) {
+			Console.WriteLine("The file '{0}' is not found.!", fileName);
+		} catch(IOException ioe) {
+			Console.WriteLine(ioe.StackTrace);
+		}
+	}
+}
